9ada1cdce98f2b2eabefbd104a3e8b68379d7f92
hey_hongh_glb <hey_hongh_glb@163.com>
2018-12-21 16:38:53 +0800

    edit by XiaoXinXin_1

--- src/main/java/com/zh/Apple.java
+++ src/main/java/com/zh/Apple.java
@@ -5,5 +5,6 @@ public class Apple {
 	public static void main(String[] args) {
 		System.out.println("test...");
 		System.out.println("develop by XiaoXinXin");
+		System.out.println("develop by XiaoXinXin_1");
 	}
 }
