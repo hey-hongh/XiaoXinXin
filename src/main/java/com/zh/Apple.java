517b1a834470dc2d14ae2680551ac5789975910d
hey_hongh_glb <hey_hongh_glb@163.com>
2018-12-30 21:11:30 +0800

    commit by hey_hongh 2018/12/30

--- src/main/java/com/zh/Apple.java
+++ src/main/java/com/zh/Apple.java
@@ -7,5 +7,6 @@ public class Apple {
 		System.out.println("develop by XiaoXinXin");
 		System.out.println("develop by XiaoXinXin_1");
 		System.out.println("develop by hey_hongh");
+		System.out.println("develop by hey_hongh 2018/12/30");
 	}
 }
